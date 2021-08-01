8a6a9ead2e6bc4794ff5805a8fa985a2b712b029
Sherin Muckatira <sherin2013007@gmail.com>
2021-08-01 14:20:42 +0000

    raw csv file

--- dataset_creation.py
+++ dataset_creation.py
@@ -7,7 +7,7 @@ import csv
 num_tokens_rep = 1
 # need to also change this value in synthetic_dataset_encoder_mlp.py
 max_seq_len = 100
-num_instances_per_seq_len = 50
+num_instances_per_seq_len = 5000
 
 x, y, y_mlp, raw_sequence, token_repeated, pos_first_token, sequence_len = generate_dataset(max_seq_len,
                                                                       num_tokens_rep, num_instances_per_seq_len)
