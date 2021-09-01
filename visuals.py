d71eb53880524dd1cf0010bb568828b180095dd2
Sherin Muckatira <sherin2013007@gmail.com>
2021-09-01 12:22:18 +0000

    Add 3d plots

--- visuals.py
+++ visuals.py
@@ -19,6 +19,7 @@ f_lstm = open('balanced_acc_seq_len_lstm.pkl', 'rb')
 acc_lstm = pickle.load(f_lstm)
 f_lstm.close()
 
+"""
 # plot the seq len vs accuracy
 x = np.arange(1,max_seq_len)
 plt.title("Balanced accuracy versus sequence length")
@@ -29,3 +30,28 @@ plot_cnn, = plt.plot(x, acc_cnn[1:100], color='blue', label='CNN')
 plot_lstm, = plt.plot(x, acc_lstm[1:100], color='magenta', label='LSTM')
 plt.legend(handles=[plot_rnn, plot_cnn, plot_lstm])
 plt.savefig("Models_acc_vs_seq_len")
+"""
+
+f_lstm = open('balanced_acc_seq_len_dist_lstm.pkl', 'rb')
+acc_lstm = pickle.load(f_lstm)
+f_lstm.close()
+
+# plot the balanced accuracy per sequence length
+# ax1 is for 3d and imshow is for 2d
+fig=plt.figure()
+ax1 = fig.add_subplot(1, 1, 1, projection='3d')
+x = np.arange(0,max_seq_len-1)
+y = np.arange(0, max_seq_len)
+xs, ys = np.meshgrid(y, x)
+plt.title("Balanced accuracy versus sequence length")
+plt.ylabel("sequence length")
+plt.xlabel("distance")
+surf = ax1.plot_surface(xs, ys, np.array(acc_lstm), cmap='plasma')
+fig.colorbar(surf, ax=ax1)
+
+#plt.imshow(acc_lstm)
+#plt.colorbar()
+plt.show()
+
+
+
