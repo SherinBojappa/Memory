729cbf756255372a420ac692aebe04fa7f14cad6
Sherin Bojappa Muckatira <sherin2013007@gmail.com>
2021-10-19 11:45:25 -0400

    Make changes to visuals.py

--- visuals.py
+++ visuals.py
@@ -1,10 +1,13 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import norm
+
 max_seq_len = 100
+#plot_type = None
 plot_type = '2d'
-#plot_type = '3d'
-#plot_type = '2d_image'
+# plot_type = '3d'
+# plot_type = '2d_image'
 # load the pickle file
 """
 # read the pickle file cnn
@@ -78,21 +81,21 @@ plt.show()
 """
 
 # plot the sequence length vs accuracy
-acc_seq_len_lstm = np.zeros(shape=(max_seq_len)) #[0]*(max_seq_len+1)
-acc_seq_len_cnn = np.zeros(shape=(max_seq_len)) #[0]*(max_seq_len+1)
-acc_seq_len_rnn = np.zeros(shape=(max_seq_len)) #[0]*(max_seq_len+1)
-acc_seq_len_trans = np.zeros(shape=(max_seq_len)) #[0]*(max_seq_len+1)
+acc_seq_len_lstm = np.zeros(shape=(max_seq_len))  # [0]*(max_seq_len+1)
+acc_seq_len_cnn = np.zeros(shape=(max_seq_len))  # [0]*(max_seq_len+1)
+acc_seq_len_rnn = np.zeros(shape=(max_seq_len))  # [0]*(max_seq_len+1)
+acc_seq_len_trans = np.zeros(shape=(max_seq_len))  # [0]*(max_seq_len+1)
 
-acc_dist_lstm = np.zeros(shape = (max_seq_len))
-acc_dist_cnn = np.zeros(shape = (max_seq_len))
-acc_dist_rnn = np.zeros(shape = (max_seq_len))
-acc_dist_trans = np.zeros(shape = (max_seq_len))
+acc_dist_lstm = np.zeros(shape=(max_seq_len))
+acc_dist_cnn = np.zeros(shape=(max_seq_len))
+acc_dist_rnn = np.zeros(shape=(max_seq_len))
+acc_dist_trans = np.zeros(shape=(max_seq_len))
 
-acc_dist_lstm = np.zeros(shape = (max_seq_len))
-acc_dist_cnn = np.zeros(shape = (max_seq_len))
-acc_dist_rnn = np.zeros(shape = (max_seq_len))
-acc_dist_trans = np.zeros(shape = (max_seq_len))
-#for seq_len_idx, acc in enumerate(acc_trans):
+acc_dist_lstm = np.zeros(shape=(max_seq_len))
+acc_dist_cnn = np.zeros(shape=(max_seq_len))
+acc_dist_rnn = np.zeros(shape=(max_seq_len))
+acc_dist_trans = np.zeros(shape=(max_seq_len))
+# for seq_len_idx, acc in enumerate(acc_trans):
 for seq_len_idx in range(1, max_seq_len):
     accuracy_lstm = 0
     accuracy_rnn = 0
@@ -102,17 +105,17 @@ for seq_len_idx in range(1, max_seq_len):
     acc_rnn = acc_model_rnn[seq_len_idx]
     acc_cnn = acc_model_cnn[seq_len_idx]
     acc_trans = acc_model_trans[seq_len_idx]
-    #for dist_idx, dist in enumerate(acc):
+    # for dist_idx, dist in enumerate(acc):
     for dist_idx in range(0, seq_len_idx):
         accuracy_lstm += acc_lstm[dist_idx]
         accuracy_cnn += acc_cnn[dist_idx]
         accuracy_rnn += acc_rnn[dist_idx]
         accuracy_trans += acc_trans[dist_idx]
-    acc_seq_len_lstm[seq_len_idx] = accuracy_lstm/(dist_idx+1)
-    acc_seq_len_rnn[seq_len_idx] = accuracy_rnn/(dist_idx+1)
-    acc_seq_len_cnn[seq_len_idx] = accuracy_cnn/(dist_idx+1)
-    acc_seq_len_trans[seq_len_idx] = accuracy_trans/(dist_idx+1)
-    #print("For given sequence length {} the accuracy is {}".format(seq_len_idx, acc_seq_len[seq_len_idx]))
+    acc_seq_len_lstm[seq_len_idx] = accuracy_lstm / (dist_idx + 1)
+    acc_seq_len_rnn[seq_len_idx] = accuracy_rnn / (dist_idx + 1)
+    acc_seq_len_cnn[seq_len_idx] = accuracy_cnn / (dist_idx + 1)
+    acc_seq_len_trans[seq_len_idx] = accuracy_trans / (dist_idx + 1)
+    # print("For given sequence length {} the accuracy is {}".format(seq_len_idx, acc_seq_len[seq_len_idx]))
 
 # plot the intermediate tokens - dist- vs accuracy
 for dist_idx in range(0, max_seq_len):
@@ -125,41 +128,44 @@ for dist_idx in range(0, max_seq_len):
         accuracy_rnn += acc_model_rnn[seq_len_idx][dist_idx]
         accuracy_cnn += acc_model_cnn[seq_len_idx][dist_idx]
         accuracy_trans += acc_model_trans[seq_len_idx][dist_idx]
-    acc_dist_lstm[dist_idx] = accuracy_lstm/(max_seq_len - dist_idx + 2)
-    acc_dist_rnn[dist_idx] = accuracy_rnn/(max_seq_len - dist_idx + 2)
-    acc_dist_cnn[dist_idx] = accuracy_cnn/(max_seq_len - dist_idx + 2)
-    acc_dist_trans[dist_idx] = accuracy_trans/(max_seq_len - dist_idx + 2)
-    print("For a given distance {} the accuracy is {}".format(dist_idx, acc_dist_lstm[dist_idx]))
-
-
+    acc_dist_lstm[dist_idx] = accuracy_lstm / (max_seq_len - dist_idx + 2)
+    acc_dist_rnn[dist_idx] = accuracy_rnn / (max_seq_len - dist_idx + 2)
+    acc_dist_cnn[dist_idx] = accuracy_cnn / (max_seq_len - dist_idx + 2)
+    acc_dist_trans[dist_idx] = accuracy_trans / (max_seq_len - dist_idx + 2)
+    print("For a given distance {} the accuracy is {}".format(dist_idx,
+                                                              acc_dist_lstm[
+                                                                  dist_idx]))
 
 if plot_type == '2d':
     # plot the seq len vs accuracy
-    x = np.arange(1,max_seq_len)
+    x = np.arange(1, max_seq_len)
     plt.title("Balanced accuracy versus sequence length")
     plt.xlabel("sequence length")
     plt.ylabel("balanced accuracy")
     plot_rnn, = plt.plot(x, acc_seq_len_rnn[1:100], color='green', label='RNN')
     plot_cnn, = plt.plot(x, acc_seq_len_cnn[1:100], color='blue', label='CNN')
-    plot_lstm, = plt.plot(x, acc_seq_len_lstm[1:100], color='magenta', label='LSTM')
-    plot_trans, = plt.plot(x, acc_seq_len_trans[1:100], color='cyan', label='TRANSFORMER')
+    plot_lstm, = plt.plot(x, acc_seq_len_lstm[1:100], color='magenta',
+                          label='LSTM')
+    plot_trans, = plt.plot(x, acc_seq_len_trans[1:100], color='cyan',
+                           label='TRANSFORMER')
     plt.legend(handles=[plot_rnn, plot_cnn, plot_lstm, plot_trans])
 
-    #plt.savefig("Models_acc_vs_seq_len")
+    # plt.savefig("Models_acc_vs_seq_len")
     plt.show()
 
     # plot the seq len vs accuracy
-    x = np.arange(0,max_seq_len-2)
+    x = np.arange(0, max_seq_len - 2)
     plt.title("Balanced accuracy versus intervening tokens")
     plt.xlabel("intervening tokens")
     plt.ylabel("balanced accuracy")
-    plot_rnn, = plt.plot(x, acc_seq_len_rnn[0:98], color='green', label='RNN')
-    plot_cnn, = plt.plot(x, acc_seq_len_cnn[0:98], color='blue', label='CNN')
+    plot_rnn, = plt.plot(x, acc_dist_rnn[0:98], color='green', label='RNN')
+    plot_cnn, = plt.plot(x, acc_dist_cnn[0:98], color='blue', label='CNN')
     plot_lstm, = plt.plot(x, acc_dist_lstm[0:98], color='magenta', label='LSTM')
-    plot_trans, = plt.plot(x, acc_dist_trans[0:98], color='cyan', label='TRANSFORMER')
+    plot_trans, = plt.plot(x, acc_dist_trans[0:98], color='cyan',
+                           label='TRANSFORMER')
     plt.legend(handles=[plot_rnn, plot_cnn, plot_lstm, plot_trans])
 
-    #plt.savefig("Models_acc_vs_seq_len")
+    # plt.savefig("Models_acc_vs_seq_len")
     plt.show()
 
 if plot_type == '3d':
@@ -181,7 +187,7 @@ if plot_type == '3d':
 
 if plot_type == '2d_image':
     fig = plt.figure()
-    #ax1 = fig.add_subplot(1, 1, 1, projection='3d')
+    # ax1 = fig.add_subplot(1, 1, 1, projection='3d')
     x = np.arange(0, max_seq_len)
     y = np.arange(0, max_seq_len)
     xs, ys = np.meshgrid(y, x)
@@ -189,11 +195,107 @@ if plot_type == '2d_image':
     plt.ylabel("sequence length")
     plt.xlabel("intervening tokens")
     # plt.zlabel("Distance ")
-    #surf = ax1.plot_surface(xs, ys, np.array(acc_model), cmap='plasma')
-    #fig.colorbar(surf, ax=ax1)
+    # surf = ax1.plot_surface(xs, ys, np.array(acc_model), cmap='plasma')
+    # fig.colorbar(surf, ax=ax1)
 
     plt.imshow(acc_model_lstm)
     plt.colorbar()
     plt.show()
 
+from scipy.optimize import curve_fit
 
+x_values = np.arange(0, 98) / 100.0
+y_values = acc_dist_lstm[0:98]
+
+
+# define the different objective functions
+def objective_line(x, a, b):
+    return a * x + b
+
+
+# objective functions from Amiri, H., et al (Eq 3- 8)
+def objective_gaussian(x, tau, b):
+    f = np.exp(-(tau * np.power(x, 2)))
+    return f + b
+
+
+def objective_lap(x, tau, b):
+    return np.exp(-(tau * x)) + b
+
+
+def objective_linear(x, tau, b):
+    return 1.0 - (tau * x * (x < 1.0 / tau)) + b
+
+
+def objective_cosine(x, tau, b):
+    return (1/2*(np.cos(tau*np.pi*x))*(x<1/tau)+1 +b)
+
+
+def objective_quadratic(x, tau,b):
+    return (1 - (tau * np.power(x, 2)*((np.power(x,2))<(1/tau)))+b)
+
+
+def objective_secant(x, tau, b):
+    return (2/(np.exp(-tau*np.power(x, 2))) + (np.exp(tau*np.power(x, 2)))+b)
+
+
+popt, _ = curve_fit(objective_line, x_values, y_values)
+a, b = popt
+print('y = %.5f * x + %.5f' % (a, b))
+
+tau_laplacian, _ = curve_fit(objective_lap, x_values, y_values)
+tau_lap, b_lap = tau_laplacian
+
+
+tau_gaussian, _ = curve_fit(objective_gaussian, x_values, y_values)
+tau_gaus, b_gaus = tau_gaussian
+
+
+tau_linear, _ = curve_fit(objective_linear, x_values, y_values)
+tau_linear, b_linear = tau_linear
+
+
+tau_cosine, _ = curve_fit(objective_cosine, x_values, y_values)
+tau_cosine, b_cosine = tau_cosine
+
+tau_quadratic, _ = curve_fit(objective_quadratic, x_values, y_values)
+tau_quadratic, b_quadratic = tau_quadratic
+
+tau_secant, _ = curve_fit(objective_secant, x_values, y_values)
+tau_secant, b_secant = tau_secant
+
+
+# plot the data points and the curve which is fitted
+plt.scatter(x_values, y_values)
+
+# calculate the output of the curve for each of the x points
+#y_curve = objective_line(x_values, a, b)
+y_gaussian = objective_gaussian(x_values, tau_gaus, b_gaus)
+y_lap = objective_gaussian(x_values, tau_lap, b_lap)
+y_linear = objective_linear(x_values, tau_linear, b_linear)
+y_cosine = objective_cosine(x_values, tau_cosine, b_cosine)
+y_quadratic = objective_quadratic(x_values, tau_quadratic, b_quadratic)
+y_secant = objective_secant(x_values, tau_secant, b_secant)
+
+
+plt.title("curve fitting to estimate memory retention curve")
+#plt_line, = plt.plot(x_values, y_curve, '--', color='red', label='line')
+
+plt_gaussian, = plt.plot(x_values, y_gaussian, '-', color='cyan',
+                         label='Gaussian')
+plt_lap, = plt.plot(x_values, y_lap, '-', color='black',
+                         label='Laplacian')
+
+plt_linear, = plt.plot(x_values, y_linear, '_', color='magenta',
+                       label='Linear')
+
+plt_cosine, = plt.plot(x_values, y_cosine, '-', color='blue',
+                       label='Cosine')
+plt_quadratic, = plt.plot(x_values, y_quadratic, '_', color='green',
+                          label='Quadratic')
+plt_secant, = plt.plot(x_values, y_secant, '_', color='red',
+                       label = 'Secant')
+
+#plt.legend(handles=[plt_line, plt_gaussian, plt_lap, plt_linear, plt_cosine, plt_secant])
+plt.legend(handles=[plt_gaussian, plt_lap, plt_linear, plt_cosine, plt_secant])
+plt.show()
